9ed48a7559763812d1ea41b1da9d13de0bf589c5
Carl Matta <carlmatta@Carls-Laptop.local>
2024-10-18 23:12:38 +0300

    New Version

--- app/src/main/java/com/usj/assignment1/MainActivity.kt
+++ app/src/main/java/com/usj/assignment1/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.usj.assignment1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var itemAdapter: ItemAdapter
+    private lateinit var showListButton: Button
+    private var itemList = mutableListOf("Harry Potter and the Sorcerer's Stone", "A Game of Thrones", "The Lord of the Rings", "The Hunger Games", "The Da Vinci Code")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        recyclerView = findViewById(R.id.recyclerView)
+        showListButton = findViewById(R.id.showListButton)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        itemAdapter = ItemAdapter(itemList) { item ->
+            onItemClick(item)
         }
+        recyclerView.adapter = itemAdapter
+
+        recyclerView.visibility = View.GONE
+
+        showListButton.setOnClickListener {
+            recyclerView.visibility = View.VISIBLE
+            showListButton.visibility = View.GONE
+        }
+    }
+
+    private fun onItemClick(item: String) {
+        Toast.makeText(this, "Clicked: $item", Toast.LENGTH_SHORT).show()
     }
 }
